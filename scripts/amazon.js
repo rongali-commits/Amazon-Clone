4ff8c56bafdc898238a1df66a44e64141cf69e2b
Chaitanya Rongali <rongalichaitanya@gmail.com>
2025-07-14 22:14:46 +0530

    Update amazon.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,8 @@
 import {cart, addToCart} from '../data/cart.js';
+
 import {products, loadProducts} from '../data/products.js';
-import {formatCurrency} from './utils/money.js';
+
+import{formatCurrency} from './utils/money.js';
 
 loadProducts(renderProductsGrid);
 
@@ -14,8 +16,6 @@ function renderProductsGrid() {
           <img class="product-image"
             src="${product.image}">
         </div>
-<div>
-</div>
 
         <div class="product-name limit-text-to-2-lines">
           ${product.name}
